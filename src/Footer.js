ced81c00039d00488b61dea2922d739f4545e7c5
MonikaTychy <mo.tychy@gmail.com>
2022-06-27 21:04:41 +0200

    Added footer with link to GitHub

---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./Footer.css";
+
+export default function Footer (){
+    return (
+    <div className="Footer">
+     <a href="https://github.com/MonikaTychy/weather-react">Open-source code</a>, by Monika Tychy
+    </div>
+    );
+}
